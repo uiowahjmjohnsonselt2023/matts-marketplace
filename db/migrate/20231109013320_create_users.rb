84e7cd6774c53f2a30738bb15ae38bb4b9bb7cbd
SergioMEV <sergiomarteloe@gmail.com>
2023-11-08 19:38:21 -0600

    Added initial scaffold for User model

---
+++ db/migrate/20231109013320_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :city
+      t.string :country
+      t.string :password_digest
+      t.string :username
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
