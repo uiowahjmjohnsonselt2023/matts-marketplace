a1c0b6fca3dd3ca739bc9b7b20444dccd06f2c46
Matthew Speranza <msperanza@uiowa.edu>
2023-11-09 13:30:17 -0600

    Add empty crud operations to ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,28 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :image_url)
+  end
 end
