653fcffaf7d0afef72f34a143ded033b72fa0905
SergioMEV <sergiomarteloe@gmail.com>
2023-11-10 13:16:30 -0600

    chore: remove old home controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,9 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  root to: 'pages#home'
 
   # Defines the root path route ("/")
-  # root "users#index" # Added this because it is required by devise, we can update this once we have an actual home page
-  # root "home#index" # Added this because it is required by devise, we can update this once we have an actual home page
-
+  root to: 'pages#home'
 
   # User auth routes
   get '\signup', to: 'users#new'
