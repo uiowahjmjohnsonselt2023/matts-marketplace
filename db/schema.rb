f9cf0f6a759d6bc7391c4c01f989e465d9591703
Haruko Okada <hokada@uiowa.edu>
2023-11-09 18:43:06 -0600

    added foreign key user_id in items and added associations in the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_09_014356) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_000836) do
+  create_table "items", force: :cascade do |t|
+    t.float "price"
+    t.string "description"
+    t.string "image_url"
+    t.string "category"
+    t.boolean "for_sale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -25,4 +37,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_09_014356) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
