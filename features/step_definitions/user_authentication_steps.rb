fef4b9c5ef54bd3b84b14475c9fae2aec67f987d
Haruko Okada <hokada@uiowa.edu>
2023-11-10 21:59:22 -0600

    test: added template for user authentication steps

---
+++ features/step_definitions/user_authentication_steps.rb
@@ -0,0 +1,108 @@
+## methods ##
+
+
+## Given ##
+Given /I am a non existing user/ do
+
+end
+
+Given /I do not exist as a user/ do
+  # Implement the code to set up an existing user, if needed
+end
+
+Given /I exist as a user/ do
+
+end
+
+
+## When ##
+When /I sign up with valid email and password/ do
+
+end
+
+When /I sign up with an existing email/ do
+
+end
+
+When /I sign up with an invalid email/ do
+
+end
+
+When /I sign up with an invalid password/ do
+
+end
+
+When /I sign up without a password confirmation/ do
+
+end
+
+When /I sign up with a mismatched password confirmation/ do
+
+end
+
+When /I sign in with email and password/ do
+
+end
+
+When /I sign in with valid email and password/ do
+
+end
+
+When /I sign in with a wrong email/ do
+
+end
+
+When /I sign in with a wrong password/ do
+
+end
+
+
+## And ##
+And /I should be redirected to the log in page/ do
+
+end
+
+And /I should be signed out/ do
+
+end
+
+And /I am not logged in/ do
+
+end
+
+
+## Then ##
+Then /I should see a successful sign up message/ do
+
+end
+
+Then /I should see a user exists message/ do
+
+end
+
+Then /I should see an invalid email message/ do
+
+end
+
+Then /I should see an invalid password message/ do
+
+end
+
+Then /I should see a missing password confirmation message/ do
+
+end
+
+Then /I should see a mismatched password message/ do
+
+end
+
+Then /I see an invalid login message/ do
+
+end
+
+Then /I see a successful sign in message/ do
+
+end
+
+
+
