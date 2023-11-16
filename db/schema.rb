246c689bd970124628b64e20a2c3e64e07b840bf
SergioMEV <sergiomarteloe@gmail.com>
2023-11-15 21:32:55 -0600

    feat: adds rating to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_10_205443) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_16_032812) do
   create_table "items", force: :cascade do |t|
     t.float "price"
     t.string "description"
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_10_205443) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.decimal "rating"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
