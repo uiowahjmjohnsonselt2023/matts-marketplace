20be29fdc803c57ae7be22bc75eb8e6a371fe233
SergioMEV <sergiomarteloe@gmail.com>
2023-11-16 00:17:11 -0600

    feat: adds featured boolean column and featured_amount_paid decimal column to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_16_052559) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_16_061601) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_052559) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "category_id"
+    t.boolean "featured"
+    t.decimal "featured_amount_paid"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
