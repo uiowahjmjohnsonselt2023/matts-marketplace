8825d81ecfc03ea473a77e307887b0e8cdbe3a97
SergioMEV <sergiomarteloe@gmail.com>
2023-11-16 00:10:15 -0600

    feat: added search by category functionality to category buttons in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @featured_items = Item.where('for_sale')
+    @all_categories = Category.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     collection do
       get 'search'
       get 'simple_search'
+      get 'category_search'
     end
   end
   resources :buyers, only: [:index, :show]
