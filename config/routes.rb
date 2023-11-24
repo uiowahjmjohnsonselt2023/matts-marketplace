8af9be13909156f0d97cb06fa09591d9dba78eb1
Mingi Lee <mingilee98@gmail.com>
2023-11-24 01:42:48 -0800

    fix: change update for profile from get to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'profile/show'
   get 'profile/edit'
-  get 'profile/update'
+  post 'profile/update'
   get 'home/index'
   resources :purchases
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
