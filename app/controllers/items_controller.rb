5e2bd4a8e38253d35ebd071bf30503f68f239124
Matthew Speranza <msperanza@uiowa.edu>
2023-11-24 16:04:44 -0600

    [style] added star to item partial for favorites list [feat] show item has link directly to chat [feat] auto gen chat w/ 'Make an offer'

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
   # GET /items or /items.json
   def index
     @items = Item.all
+    @items = @items.where(for_sale: true)
   end
 
   # GET /items/1 or /items/1.json
