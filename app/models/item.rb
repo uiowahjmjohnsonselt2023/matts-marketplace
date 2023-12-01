7935584407e69e23e92f0a640f8600a13a5e1dab
Matthew Speranza <msperanza@uiowa.edu>
2023-11-30 22:28:19 -0600

    [fix] SQL search [fix] Catagory failure in advanced search where an array [nil] was passed [fix] search.html.erb to include path_keyword when rendering partial

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,12 +23,18 @@ class Item < ApplicationRecord
         price_high = price_range[1].to_i
       end
     end
-    items = Item.where("price >= ? AND price <= ? AND for_sale == ?", price_low, price_high, for_sale)
+    items = 0
+    if for_sale
+      items = Item.where("price >= ? AND price <= ? AND for_sale", price_low, price_high)
+    else
+      items = Item.where("price >= ? AND price <= ? AND NOT for_sale", price_low, price_high)
+    end
     if !categories.nil? && !categories.empty?
       categories.each do |category|
-        category_object = Category.where("name == (?)", Item.sanitize_sql_like(category)).first
-        # Not sure how important the sanitize_sql_like is here, but it's probably a good idea
-        items = items.and(category_object.items) unless category.empty?
+        if !category.nil?
+          category_object = Category.where("name == (?)", category).first
+          items = items.and(category_object.items) unless category.empty?
+        end
       end
     end
     if !terms.nil? && !terms.empty?
