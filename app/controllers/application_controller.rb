8cf017a5259574c18f86580e0400851877fdb12c
Matthew Speranza <msperanza@uiowa.edu>
2023-12-01 00:01:03 -0600

    [style] Improved look of wishlist [style] sidebar improvements in profile_controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_sidebar
+
+  private
+  def set_sidebar
+    @sidebar_present = ["profile"].include?(params[:controller])
+  end
 end
