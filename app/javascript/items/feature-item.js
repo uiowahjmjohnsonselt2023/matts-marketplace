15d2b448ee1e0752590e4479f772f4dd6e5b0a5b
Haruko Okada <hokada@uiowa.edu>
2023-12-06 17:45:35 -0600

    feature: implemented javascript to only show textbox when checkbox is checked

---
+++ app/javascript/items/feature-item.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const featureCheckbox = document.getElementById('featured-checkbox');
+    const featuredAmountField = document.getElementById('featured-amount-field');
+
+    // Function to toggle the display of the featured amount field
+    function toggleFeaturedAmountField() {
+        if (featureCheckbox.checked) {
+            featuredAmountField.style.display = 'block';
+        } else {
+            featuredAmountField.style.display = 'none';
+        }
+    }
+
+    // Initial check on page load
+    toggleFeaturedAmountField();
+
+    // Event listener for checkbox change
+    featureCheckbox.addEventListener('change', toggleFeaturedAmountField);
+});
