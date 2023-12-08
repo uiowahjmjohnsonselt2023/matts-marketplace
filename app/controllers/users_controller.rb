f2b221dd5893a4caba34628f51d8fb4adf42b703
Mingi Lee <mingilee98@gmail.com>
2023-12-08 02:26:33 -0600

    feat: add editing users for manage users feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,10 +41,10 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+        format.html { redirect_to admin_manage_users_url, notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit, status: :unprocessable_entity, notice: "User update failed."  }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -68,7 +68,7 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :city, :country, :password_digest, :username, :email)
+      params.require(:user).permit(:first_name, :last_name, :city, :country, :password_digest, :username, :email, :balance, :rating, :admin)
     end
 
     def require_admin
