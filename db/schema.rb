40078da263f8ae899c9c90e2056af393dccbe606
Mingi Lee <mingilee98@gmail.com>
2023-12-09 16:40:33 -0600

    feat: add root_admin column to user model via rails generate migration AddRootAdminToUsers root_admin:boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_09_211159) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_09_223856) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -105,6 +105,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_09_211159) do
     t.boolean "admin", default: false
     t.boolean "banned", default: false
     t.string "image_url", default: ""
+    t.boolean "root_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
