6ae4d58339edf2c5d91794cc6456b37e6a764edb
Mingi Lee <mingilee98@gmail.com>
2023-12-09 13:24:28 -0600

    feat: add a migration to add banned field to users via command 'rails generate migration AddBannedToUsers banned:boolean'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_07_090700) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_09_192243) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_090700) do
     t.decimal "rating"
     t.decimal "balance", default: "0.0"
     t.boolean "admin", default: false
+    t.boolean "banned", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
